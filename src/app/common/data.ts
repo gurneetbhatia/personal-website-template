63720c3ee446a6dd4fcebbc939b0769d736ab158
Gurneet Bhatia <sbgurneet@gmail.com>
2020-08-21 21:40:30 +0400

    designed the competition card

--- src/app/common/data.ts
+++ src/app/common/data.ts
@@ -15,8 +15,8 @@ export const competitions: Competition[]  = [
 
     {
         title: "Some award",
-        description: `About the award and its prestige`,
+        description: `About the award and its prestige (you can also add images like this)`,
         date: "March, 2020",
-        photoUrl: "https://i.ibb.co/2h8qY4T/award3.jpg"
+        photoUrl: "https://techcrunch.com/wp-content/uploads/2019/06/GettyImages-944060114.jpg?w=730&crop=1"
     }
 ]
--- src/app/pages/competitions/competition-card/competition-card.component.ts
+++ src/app/pages/competitions/competition-card/competition-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Competition } from '../competition';
 
 @Component({
   selector: 'app-competition-card',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./competition-card.component.css']
 })
 export class CompetitionCardComponent implements OnInit {
+
+  @Input() competition: Competition;
 
   constructor() { }
 
