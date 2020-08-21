09fb7c0dd4f44c32e669e9626bc7599cf7472523
Gurneet Bhatia <sbgurneet@gmail.com>
2020-08-21 19:48:00 +0400

    defined the routes for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { CompetitionsComponent } from './pages/competitions/competitions.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'competitions', component: CompetitionsComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
