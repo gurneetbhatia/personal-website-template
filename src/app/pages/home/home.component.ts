508a02c93e42de0297573f679368285cc3059d02
Gurneet Bhatia <sbgurneet@gmail.com>
2020-08-21 20:08:06 +0400

    designed the home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+//import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  @ViewChild('gitStatsContainer') gitStatsContainer: ElementRef;
+  gitStats = ``;
+
   constructor() { }
 
   ngOnInit(): void {
+    // this.dataService.getGitStats().subscribe(
+    //   (resp) => {
+    //     console.log("here")
+    //     this.gitStats = resp;
+    //     this.gitStatsContainer.nativeElement.innerHTML = this.gitStats;
+    //   },
+    //   (err) => {
+    //     console.log(err);
+    //   }
+    // )
   }
 
 }
